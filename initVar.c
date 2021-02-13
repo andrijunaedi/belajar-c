4c9b7be12081823b39a5b8328f2e7891f855209c
andrijunaedi <andrijunaedi126@gmail.com>
2021-02-13 14:59:25 +0700

    init and assign float variable

--- initVar.c
+++ initVar.c
@@ -2,11 +2,19 @@
 
 int main(){
     int i;
+    float f = 0.555;
+    float x = 1.5E3;
 
     i = 5;
     printf("Ini nilai i : %d \n", i);
 
     i = i + 1;
     printf("Ini nilai i : %d \n", i);
+
+    // Init float variable
+    printf("Ini nilai f : %f \n", f);
+    printf("Ini nilai f : %5.2f \n", f);
+    printf("Ini nilai x : %10.2f \n", f);
+
     return 0;
 }
