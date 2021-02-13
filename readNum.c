426b5facc3b183b96c919ba647c80d4e647ec2ff
andrijunaedi <andrijunaedi126@gmail.com>
2021-02-13 16:00:38 +0700

    read and write numeric value

---
+++ readNum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    float x;
+
+    printf("Contoh membaca dan menulis, ketik nilai integer : \n");
+    scanf("%d", &a);
+    printf("Nilai yang dibaca %d \n", a);
+
+    printf("Ketik nilai sebuah bilangan float : \n");
+    scanf ("%f", x);
+    printf("Nilai yang dibaca %d \n", x);
+
+    return 0;
+}
