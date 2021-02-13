ce1387c8d921d4d5588d8116fd5df8c864b691ee
andrijunaedi <andrijunaedi126@gmail.com>
2021-02-13 19:29:13 +0700

    conditional - multiple conditional

--- conditional.c
+++ conditional.c
@@ -7,8 +7,10 @@ int main(){
     printf("Ketikkan satu nilai integer :");
     scanf("%d", &a);
 
-    if (a >= 0){
+    if (a > 0){
         printf("Nilai a positif %d \n", a);
+    } else if (a == 0){
+        printf("Nilai Nol %d \n", a);
     } else {
         printf("Nilai a negatif %d \n", a);
     }
