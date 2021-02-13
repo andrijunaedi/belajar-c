e689e0f0a3cea3e9a4805a40fc0029617e169430
andrijunaedi <andrijunaedi126@gmail.com>
2021-02-13 16:29:45 +0700

    read and write chac value

---
+++ readChar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    char cc;
+    char c;
+
+    printf ("hello\n");
+    printf("baca 1 karakter saja : ");scanf ("%c ", &cc);
+    printf("baca 1 karakter saja : ");scanf ("%d ", &c); 
+    printf ("%c \n", cc);
+    printf ("%d \n", c);
+    
+    return 0;
+}
